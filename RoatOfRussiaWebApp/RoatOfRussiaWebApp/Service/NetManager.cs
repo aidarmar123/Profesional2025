0a0a1cd7cf4ccaaa65db63119250b2150ef0e462
Aidar <ajdar.mar@yandex.ru>
2025-01-13 21:34:32 +0300

    Web App on Blazor

---
+++ RoatOfRussiaWebApp/RoatOfRussiaWebApp/Service/NetManager.cs
@@ -0,0 +1,20 @@
+﻿
+using Newtonsoft.Json;
+
+namespace RoatOfRussiaWebApp.Service
+{
+    public static class NetManager
+    {
+        public static string URL = "http://localhost:62367/";
+        public static HttpClient httpClient = new HttpClient();
+
+        public static async Task<T> Get<T>(string path)
+        {
+            var response = await httpClient.GetAsync(URL+path);
+            var content = await response.Content.ReadAsStringAsync();
+            var data =  JsonConvert.DeserializeObject<T>(content);
+            return data;
+        }
+       
+    }
+}
